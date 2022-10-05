206e2e0144e1cfd9ce1ca20fdd2e1ba19ed1ffa2
terp1s <90653457+terp1s@users.noreply.github.com>
2022-10-05 16:03:07 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Premier
+    {
+        public string Jmeno;
+        public string SilneProhlaseni()
+        {
+            throw new NotImplementedException();
+        }
+        public Vlada Vlad;
+
+    }
+
+    class Ministr
+    {
+        public Ministr Min;
+        public string Jmeno;
+        public string SlabeProhlaseni()
+        {
+            return "No já tedy nevím, ale premiér by řekl" + Prem.SilneProhlaseni();
+        }
+        public Premier Prem;
+        private void Demise()
+        {
+            Prem.Vlad.Ministerstvo.Remove(Min);
+        }
+    }
+
+    class Vlada
+    {
+        public Premier Prem;
+        public List <Ministr> Ministerstvo;
+        public string Zeme;
+        bool ZkusAnektovat(string obet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
